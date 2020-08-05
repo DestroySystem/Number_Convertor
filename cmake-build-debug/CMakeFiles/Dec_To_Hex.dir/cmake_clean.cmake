72d6346fe1a2d08ba62d7e9e05794ad67bc9e508
SystemDestroy <lorddarckarz@gmail.com>
2020-08-05 23:39:50 +0300

    Convertor

---
+++ cmake-build-debug/CMakeFiles/Dec_To_Hex.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dec_To_Hex.dir/main.cpp.obj"
+  "Dec_To_Hex.exe"
+  "Dec_To_Hex.exe.manifest"
+  "Dec_To_Hex.pdb"
+  "libDec_To_Hex.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dec_To_Hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
