88a0414088893fc122001574b2763780a8ff0ceb
SystemDestroy <lorddarckarz@gmail.com>
2020-08-07 12:03:45 +0300

    Program to convert decimal number to octal and  hexadecimal number

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
+int Bin_To_Hex(int value)
+{
+    int array[10], i;
+    for(i=0; value > 0; i++)
+    {
+        array[i] = value % 2;
+        value = value / 2;
+    }
+    for(i = i - 1 ; i >= 0; i--)
+    {
+        cout << array[i];
+    }
+    return array[i];
+}
 int main()
 {
    int dec = 0;
-   cout << "Enter number: " << endl;
+   cout << "Enter number which you want to convert: " << endl;
    cin >> dec;
    cout.setf(ios_base::showbase);
-   cout << " Hex value of decimal number " << dec << " in hexadecimal is: " << hex << dec << endl;
+   cout << " Hexadecimal value of decimal number " << dec << " is: " << hex << dec << endl;
+   cout.unsetf(ios_base::hex);
+   cout.unsetf(ios_base::showbase);
+   cout << " Octal value of decimal number " << dec << " is: " << oct << dec << endl;
+   cout.unsetf(ios_base::oct);
+   cout << " Binary value of decimal number " << dec << " is: " << Bin_To_Hex(dec) << endl;
 }
